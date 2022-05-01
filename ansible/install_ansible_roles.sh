1eb5446941b2a5164f0b0248970a2371c792a08c
Kris Urbanski <kris@whereibend.space>
2022-05-01 11:20:07 +0200

    Update tmux.conf, ansible install roles

---
+++ ansible/install_ansible_roles.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+ansible-galaxy install openprivacy.simple-harden
+ansible-galaxy install likg.apt-repo-sury-org
+ansible-galaxy install blackbaud.linux-hardening
+ansible-galaxy install grzegorznowak.nvm_node
+ansible-galaxy install githubixx.harden-linux
+
+ansible-galaxy install konstruktoid.hardening
+
+ansible-galaxy collection install community.dns
+
+ansible-galaxy collection install community.docker
+
+ansible-galaxy collection install community.general
+
+ansible-galaxy collection install community.google
+
+ansible-galaxy collection install community.grafana
+
+ansible-galaxy collection install community.network
+
+ansible-galaxy collection install community.crypto
+
+ansible-galaxy collection install buluma.workstation
+
+ansible-galaxy collection install buluma.virtualization
+
+ansible-galaxy collection install buluma.roles
+
+ansible-galaxy collection install robertdebock.roles
+
+
+
+ansible-galaxy collection install community.general
+
+ansible-galaxy install iroquoisorg.nginx_vhost
+
+ansible-galaxy install robertdebock.php_fpm
+
+ansible-galaxy install buluma.php
+
+ansible-galaxy install robertdebock.php
+
+ansible-galaxy install juwai.laravel
+
+ansible-galaxy collection install geerlingguy.php_roles
+
+ansible-galaxy install geerlingguy.php
+
+ansible-galaxy install geerlingguy.composer
+
+ansible-galaxy install geerlingguy.nginx
+
+ansible-galaxy install geerlingguy.mysql
+
+ansible-galaxy install geerlingguy.git
+
+ansible-galaxy install geerlingguy.redis
+
+ansible-galaxy install geerlingguy.firewall
+
+ansible-galaxy install geerlingguy.security
+
+ansible-galaxy install geerlingguy.nodejs
+
+ansible-galaxy install geerlingguy.mailhog
+
+ansible-galaxy install geerlingguy.certbot
+
+ansible-galaxy install geerlingguy.php-mysql
+
+ansible-galaxy install geerlingguy.php-versions
+
+ansible-galaxy install geerlingguy.phpmyadmin
+
+ansible-galaxy install geerlingguy.php-pgsql
+
+
+
+ansible-galaxy install geerlingguy.backup
+
+ansible-galaxy install geerlingguy.adminer
+
+ansible-galaxy install geerlingguy.php-memcached
+
+ansible-galaxy install geerlingguy.docker
+
+ansible-galaxy install geerlingguy.pip
+
+ansible-galaxy install geerlingguy.memcached
+
+ansible-galaxy install geerlingguy.ntp
+
+ansible-galaxy install geerlingguy.postgresql
+
+ansible-galaxy install geerlingguy.jenkins
+
+ansible-galaxy install geerlingguy.dotfiles
