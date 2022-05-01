9880d4d039ea342a7825e7b549ded00f3349cb0f
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2022-05-01 19:28:23 +0200

    Update dotfiles

--- install_dots.sh
+++ install_dots.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# install ubuntu packages
-cat packages/apt_packages | xargs sudo apt -mfy install
-
-# install python packages
-pip3 install -r packages/python3_packages
-
-source packages/npm_packages
-
-cp -fr .* ~/
-cp -fr bin ~/
-
-# make folders
+# make important folders
 mkdir ~/archive ~/docs ~/workshops ~/library ~/temp ~/Portal ~/sync
+mkdir -p ~/.config/composer ~/.local/share/fonts
+mkdir ~/.fonts
+
+
+# link folders
+ln -sf $(pwd)/.doom.d $HOME/.doom.d
+ln -sf $(pwd)/.fonts $HOME/.fonts
+ln -sf $(pwd)/.fonts $HOME/.local/share/fonts
+
+# install doom emacs
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+# make symbolic links to dotfiles  -> one source of truth
+find . -maxdepth 1 -type f -name ".*" | while read dotfile; do clean_name="${dotfile/.\//}"; ln -sf $(pwd)/$clean_namea $HOME/$clean_name; done
